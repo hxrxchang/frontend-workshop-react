d3ab9b526ee98f0a7444a1eef16b4ceeaf407df1
hxrxchang <mail@hxrxchang.dev>
2024-03-28 16:23:33 +0900

    impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FormEvent, useRef, useState } from 'react'
 import './App.css'
 
+type Task = {
+  id: string
+  name: string
+  done: boolean
+}
+
+function genId() {
+  return crypto.randomUUID()
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: genId(), name: 'hoge', done: true },
+  ])
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const name = inputRef.current?.value
+    if (name) {
+      const newTasks = [...tasks, { id: genId(), name, done: false }]
+      setTasks(newTasks)
+      inputRef.current.value = ''
+    }
+  }
+
+  const [onlyNotDone, setOnlyNotDone] = useState(false)
+
+  const filteredTasks = onlyNotDone ? tasks.filter((task) => !task.done) : tasks
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>TOOO</h1>
+      <h2>My Tasks</h2>
+      show only not finished tasks ?
+      <input
+        type="checkbox"
+        checked={onlyNotDone}
+        onChange={() => setOnlyNotDone(!onlyNotDone)}
+      />
+      {filteredTasks.length === 0 ? (
+        <p>no tasks</p>
+      ) : (
+        <ul>
+          {filteredTasks.map((task, _) => (
+            <li key={task.id}>
+              <div>
+                <input
+                  type="checkbox"
+                  checked={task.done}
+                  onChange={() => {
+                    const newTasks = tasks.map((t) => {
+                      if (t.id === task.id) {
+                        return { ...t, done: !t.done }
+                      }
+                      return t
+                    })
+                    setTasks(newTasks)
+                  }}
+                />
+                <span>{task.name}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="write task" ref={inputRef} />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
   )
 }
 
